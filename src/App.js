e1a8f24d1a82fa5faea55ba97c44070d19cb97e8
Arpit Surana <arpit@permissionles.org>
2022-11-02 16:28:10 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -11,13 +11,6 @@
 */
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
-import Tables from "./pages/Tables";
-import Billing from "./pages/Billing";
-import Rtl from "./pages/Rtl";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
