4a88c849738ba5f9a66a8e6949ea96689a3cff38
Andyprmsnls <anuj@prmsnls.xyz>
2022-11-02 14:59:17 +0530

    sidebar removed top header removed

--- src/App.js
+++ src/App.js
@@ -26,16 +26,9 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
-        <Main>
-          <Route exact path="/dashboard" component={Home} />
-          <Route exact path="/tables" component={Tables} />
-          <Route exact path="/billing" component={Billing} />
-          <Route exact path="/rtl" component={Rtl} />
-          <Route exact path="/profile" component={Profile} />
-          <Redirect from="*" to="/dashboard" />
-        </Main>
+        <Route exact path="/dashboard" component={Home} />
+        <Redirect from="*" to="/dashboard" />
+        {/* <Main></Main> */}
       </Switch>
     </div>
   );
